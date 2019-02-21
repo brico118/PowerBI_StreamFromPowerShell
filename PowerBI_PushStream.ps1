5098d480cde35d0cbc9413885bd083a2be25b71c
brico118 <39496005+brico118@users.noreply.github.com>
2019-02-20 20:32:39 -0500

    Add files via upload

---
+++ PowerBI_PushStream.ps1
@@ -0,0 +1,28 @@
+﻿$SqlServer = '<ServerName>';
+$SqlDatabase = '<DatabaseName>';
+
+$SqlConnectionString = "Server=$SqlServer;Database=$SqlDatabase;Integrated Security=True;"
+$SqlQuery = "<Query>'"
+
+$SqlCommand = New-Object -TypeName System.Data.SqlClient.SqlCommand;
+$SqlCommand.CommandText = $SqlQuery;
+$SqlConnection = New-Object -TypeName System.Data.SqlClient.SqlConnection -ArgumentList $SqlConnectionString;
+$SqlCommand.Connection = $SqlConnection;
+
+$SqlConnection.Open();
+$SqlDataReader = $SqlCommand.ExecuteReader();
+
+
+$endpoint = "<URL to Push data>"
+
+while ($SqlDataReader.Read()) {
+    $payload = 
+    @{
+    "<Key>" =$SqlDataReader['<Value>']
+    
+    }
+    Invoke-RestMethod -Method Post -Uri "$endpoint" -Body (ConvertTo-Json @($payload))
+}
+
+$SqlConnection.Close();
+$SqlConnection.Dispose();
